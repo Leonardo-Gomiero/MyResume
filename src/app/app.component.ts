07cf7c7232194b4e41532a21215d67952ae51a42
Leonardo Gomiero <leoleonardo2000@gmail.com>
2023-12-12 21:03:38 -0300

    Footer structured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faGithub,faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -21,5 +22,7 @@ export class AppComponent{
   title = 'MyResume';
   github = faGithub;
   linkedin = faLinkedin;
+  whatsapp = faWhatsapp;
+  email = faEnvelope;
 
 }
